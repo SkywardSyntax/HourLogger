dbe7a73c127a84766947d7a081e98bb268b22e7c
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2023-11-10 02:47:27 +0000

    Removed Debugging Statements

--- HoursAdder.py
+++ HoursAdder.py
@@ -3,7 +3,6 @@ def calculate_total_time():
     with open('attendance.txt', 'r') as f:
         for line in f:
             line = line.strip()  # Strip newline character
-            print(f"Reading line: {line}")  # Debugging print statement
             parts = line.split()
             if len(parts) == 5:  # id, hours, number, minutes, number
                 id = parts[0]
@@ -13,7 +12,7 @@ def calculate_total_time():
                     totals[id] = {'hours': 0, 'minutes': 0}
                 totals[id]['hours'] += hours
                 totals[id]['minutes'] += minutes
-                print(f"Updated totals: {totals}")  # Debugging print statement
+
 
     # Convert minutes to hours
     for id in totals:
@@ -24,7 +23,7 @@ def calculate_total_time():
     return totals
 
 totals = calculate_total_time()
-print(f"Final totals: {totals}")  # Debugging print statement
+
 
 with open('hourTotals.txt', 'w') as f:
     for id, time in totals.items():
