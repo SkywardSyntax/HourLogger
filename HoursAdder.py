8cf1133a2b883b7889739a4c5f0f57c926a6566c
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2023-11-11 03:33:59 +0000

    Added sheetExporter.py for easy .csv exporting Improvements to HoursAdder.py

--- HoursAdder.py
+++ HoursAdder.py
@@ -1,5 +1,18 @@
+import re
+import os
+
 def calculate_total_time():
     totals = {}
+    # Read existing totals from hourTotals.txt
+    if os.path.exists('hourTotals.txt'):
+        with open('hourTotals.txt', 'r') as f:
+            for line in f:
+                match = re.match(r'ID: (\d+), Total time: (\d+) hours (\d+) minutes', line.strip())
+                if match:
+                    id, hours, minutes = match.groups()
+                    totals[id] = {'hours': int(hours), 'minutes': int(minutes)}
+
+    # Add new totals from attendance.txt
     with open('attendance.txt', 'r') as f:
         for line in f:
             line = line.strip()  # Strip newline character
@@ -13,17 +26,18 @@ def calculate_total_time():
                 totals[id]['hours'] += hours
                 totals[id]['minutes'] += minutes
 
-
     # Convert minutes to hours
     for id in totals:
         hours, minutes = divmod(totals[id]['minutes'], 60)
         totals[id]['hours'] += hours
         totals[id]['minutes'] = minutes
 
+    # Clear attendance.txt
+    open('attendance.txt', 'w').close()
+
     return totals
 
 totals = calculate_total_time()
-
 
 with open('hourTotals.txt', 'w') as f:
     for id, time in totals.items():
---
+++ sheetExporter.py
@@ -0,0 +1,17 @@
+import csv
+import re
+
+with open('hourTotals.txt', 'r') as f:
+    lines = f.readlines()
+
+data = []
+for line in lines:
+    match = re.match(r'ID: (\d+), Total time: (\d+) hours (\d+) minutes', line.strip())
+    if match:
+        id, hours, minutes = match.groups()
+        data.append([id, f"{int(hours):02d}:{int(minutes):02d}:00"])
+
+with open('hourTotals.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['ID', 'Total Time'])
+    writer.writerows(data)
