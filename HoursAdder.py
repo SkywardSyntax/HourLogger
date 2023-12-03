232b9f2b535f023207b81afb1fd08907a09da19f
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2023-12-03 06:42:52 +0000

    Increased Fault Tolerance of Application

--- HoursAdder.py
+++ HoursAdder.py
@@ -1,8 +1,11 @@
 import re
 import os
+import shutil
+import datetime
 
 def calculate_total_time():
     totals = {}
+    suspicious_activity = []
     # Read existing totals from hourTotals.txt
     if os.path.exists('hourTotals.txt'):
         with open('hourTotals.txt', 'r') as f:
@@ -16,11 +19,14 @@ def calculate_total_time():
     with open('attendance.txt', 'r') as f:
         for line in f:
             line = line.strip()  # Strip newline character
-            parts = line.split()
-            if len(parts) == 5:  # id, hours, number, minutes, number
-                id = parts[0]
-                hours = int(parts[1])
-                minutes = int(parts[3])
+            match = re.match(r'(\d+) Checked In at .+ and Checked Out at .+, Meeting Time Recorded: (\d+) hours (\d+) minutes', line)
+            if match:
+                id, hours, minutes = match.groups()
+                hours = int(hours)
+                minutes = int(minutes)
+                if hours >= 10:
+                    suspicious_activity.append(line)
+                    continue
                 if id not in totals:
                     totals[id] = {'hours': 0, 'minutes': 0}
                 totals[id]['hours'] += hours
@@ -32,8 +38,19 @@ def calculate_total_time():
         totals[id]['hours'] += hours
         totals[id]['minutes'] = minutes
 
+    # Archive attendance.txt
+    if not os.path.exists('Archives'):
+        os.makedirs('Archives')
+    today = datetime.date.today()
+    shutil.copy('attendance.txt', f'Archives/Archive {today.month}-{today.day}-{today.year}.txt')
+
     # Clear attendance.txt
     open('attendance.txt', 'w').close()
+
+    # Write suspicious activity to file
+    with open('suspicious_activity.txt', 'w') as f:
+        for line in suspicious_activity:
+            f.write(line + '\n')
 
     return totals
 
--- HoursWeb.py
+++ HoursWeb.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, Response
 import datetime
 import re
 import random
@@ -10,65 +11,96 @@ random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=3
 r_string = ("/" + random_string)
 
 class Attendance:
-
-    
     def __init__(self):
         self.records = {}
-        self.line_numbers = {}
 
     def check_in(self, id):
         now = datetime.datetime.now()
-        if id not in self.records or 'check_in_time' not in self.records[id]:
-            self.records[id] = {'total_time': datetime.timedelta(), 'check_in_time': now}
+        if id not in self.records or 'check_out_time' in self.records[id]:
+            self.records[id] = {'check_in_time': now}
             with open('attendance.txt', 'a') as f:
-                f.write(f"{id}\n")
-            with open('attendance.txt', 'r') as f:
-                lines = f.readlines()
-            self.line_numbers[id] = len(lines) - 1
+                f.write(f"{id} Checked In at {now}\n")
             return "Checked In!"
         else:
             return "Already Checked In!"
 
-
     def check_out(self, id):
         now = datetime.datetime.now()
-        if id in self.records and 'check_in_time' in self.records[id]:
-            check_in_time = self.records[id]['check_in_time']
-            time_diff = now - check_in_time
-            self.records[id]['total_time'] += time_diff
-            del self.records[id]['check_in_time']
+        check_in_time = None
 
-            # Convert total_time from seconds to minutes and hours
-            total_seconds = self.records[id]['total_time'].total_seconds()
+        # Check if the user has an incomplete entry in the attendance.txt file
+        with open('attendance.txt', 'r') as f:
+            lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith(f"{id} Checked In") and "Checked Out" not in line:
+                check_in_time_str = line.split(" at ")[1].strip()
+                check_in_time = datetime.datetime.strptime(check_in_time_str, "%Y-%m-%d %H:%M:%S.%f")
+                break
+
+        if check_in_time is not None or (id in self.records and 'check_in_time' in self.records[id]):
+            if check_in_time is None:
+                check_in_time = self.records[id]['check_in_time']
+            time_diff = now - check_in_time
+
+            # Convert time_diff from seconds to minutes and hours
+            total_seconds = time_diff.total_seconds()
             hours, remainder = divmod(total_seconds, 3600)
             minutes, _ = divmod(remainder, 60)
 
-            # Read the file and update the relevant line
-            with open('attendance.txt', 'r') as f:
-                lines = f.readlines()
-            lines[self.line_numbers[id]] = f"{id} {int(hours)} hours {int(minutes)} minutes\n"
+            self.records[id] = {'check_out_time': now}
+
+            # Update the relevant line
+            for i, line in enumerate(lines):
+                if line.startswith(f"{id} Checked In"):
+                    lines[i] = f"{id} Checked In at {check_in_time} and Checked Out at {now}, Meeting Time Recorded: {int(hours)} hours {int(minutes)} minutes\n"
+                    break
+
             # Write the updated content back to the file
             with open('attendance.txt', 'w') as f:
                 f.writelines(lines)
-            return f"Checked Out! \nMeeting Time Recorded: {int(hours)} hours {int(minutes)} minutes"
-
-
-
-    def get_total_time(self, id):
-        if id in self.records:
-            return self.records[id]['total_time']
+            return f"Checked Out! Meeting Time Recorded: {int(hours)} hours {int(minutes)} minutes"
         else:
-            return None
+            return "Not Checked In!"
 
     def check_status_and_act(self, id):
-        if id in self.records and 'check_in_time' in self.records[id]:
+        now = datetime.datetime.now()
+
+        # Check if the user has an incomplete entry in the attendance.txt file
+        with open('attendance.txt', 'r') as f:
+            lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith(f"{id} Checked In") and "Checked Out" not in line:
+                check_in_time_str = line.split(" at ")[1].strip()
+                check_in_time = datetime.datetime.strptime(check_in_time_str, "%Y-%m-%d %H:%M:%S.%f")
+                time_diff = now - check_in_time
+
+                # Convert time_diff from seconds to minutes and hours
+                total_seconds = time_diff.total_seconds()
+                hours, remainder = divmod(total_seconds, 3600)
+                minutes, _ = divmod(remainder, 60)
+
+                if hours >= 10:
+                    # If it's been more than 10 hours, check them out and check them in again
+                    lines[i] = f"{id} Checked In at {check_in_time} and Checked Out at {now}, Meeting Time Recorded: {int(hours)} hours {int(minutes)} minutes\n"
+                    with open('attendance.txt', 'w') as f:
+                        f.writelines(lines)
+                    return self.check_in(id)
+                else:
+                    return self.check_out(id)
+
+        # If the user doesn't have an incomplete entry, proceed as before
+        if id in self.records and 'check_in_time' in self.records[id] and 'check_out_time' not in self.records[id]:
             return self.check_out(id)
         else:
             return self.check_in(id) if re.match(r'^\d{6}$', id) else "Invalid Student Id Number. Please enter only the 6 digit number."
 
-
-
 attendance = Attendance()  # Create an instance of Attendance
+
+@app.route('/raw_hours', methods=['GET'])
+def attendance_data():
+    with open('attendance.txt', 'r') as f:
+        data = f.read()
+    return Response(data, mimetype='text/plain')
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -87,6 +119,7 @@ def home():
         id = request.form.get('id')
         message = attendance.check_status_and_act(id)
     return render_template('home.html', message=message)
+
 
 def main():
     app.run(debug=True)
